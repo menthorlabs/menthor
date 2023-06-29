fd6db6ce436fde9878ba97ec41af6e1b068e37a1
Bernardo Simonassi <bernardosimonassi@hotmail.com>
2023-06-28 23:34:49 -0300

    feat: next/prev lessons and tags

--- apps/app/components/atoms/AppIconButton.vue
+++ apps/app/components/atoms/AppIconButton.vue
@@ -1,10 +1,36 @@
+<script setup lang="ts">
+const { app = "tailwind" } = defineProps<{
+  app?: string;
+}>();
+
+const apps: { [key: string]: { url: string } } = {
+  vercel: {
+    url: "https://vercel.com/?utm_source=menthor.io",
+  },
+  vue: {
+    url: "https://vuejs.org/?utm_source=menthor.io",
+  },
+  tailwind: {
+    url: "https://tailwindcss.com/?utm_source=menthor.io",
+  },
+  github: {
+    url: "https://github.com/?utm_source=menthor.io",
+  },
+  ethers: {
+    url: "https://ethers.org/?utm_source=menthor.io",
+  },
+};
+</script>
+
 <template>
-  <div
+  <nuxt-link
+    :to="apps[app].url"
+    target="_blank"
     class="flex h-[40px] w-[40px] min-w-[40px] cursor-pointer items-center justify-center rounded-full border border-zinc-200 bg-white/90 px-2 py-3 hover:bg-zinc-100/90"
   >
     <img
-      src="/logos/tailwind.svg"
+      :src="`/logos/${app}.svg`"
       alt="w-full h-full object-contain object-center"
     />
-  </div>
+  </nuxt-link>
 </template>
--- apps/app/pages/[...slug].vue
+++ apps/app/pages/[...slug].vue
@@ -6,26 +6,44 @@ const sessionStore = useSessionStore();
 const enrollmentModalStore = useEnrollmentModalStore();
 const { params } = useRoute();
 const finishLesson = ref(false);
+const router = useRouter();
+const isEnrolled = ref(true);
 
 const { data } = await useAsyncData("course", async () => {
-  const [content, surround] = await Promise.all([
+  const [course, lesson, surround] = await Promise.all([
     queryContent(`/${params.slug[0]}`).findOne(),
+    queryContent(
+      `/${params.slug[0]}/${params.slug[1]}/${params.slug[2]}`
+    ).findOne(),
     queryContent()
       .where({ _type: "markdown" })
       .findSurround(`/${params.slug[0]}/${params.slug[1]}/${params.slug[2]}`),
   ]);
-  return { data: { content, surround } };
+  return { data: { course, lesson, surround } };
 });
 
-console.log(data.value.data.surround);
-const { description, image, title } = data.value.data.content._dir;
+const previousLesson = data.value.data.surround[0];
+const nextLesson = data.value.data.surround[1];
+const currentLesson = data.value.data.lesson;
+
+const { description, image, title, tags } = data.value.data.course._dir;
+
+function goToNextLesson() {
+  finishLesson.value = false;
+  router.push(nextLesson._path);
+}
 </script>
 
 <template>
   <main class="relative">
     <ClientOnly>
       <Teleport to="#modals">
-        <MModal v-model="finishLesson" class="w-full max-w-[480px]" footer>
+        <MModal
+          v-model="finishLesson"
+          class="w-full max-w-[480px]"
+          @confirm="goToNextLesson"
+          footer
+        >
           <template #header> Finalizar aula </template>
           <template #description>
             Envie seu progresso antes de continuar
@@ -57,38 +75,41 @@ const { description, image, title } = data.value.data.content._dir;
           {{ description }}
         </p>
         <div class="flex items-center gap-2">
-          <AppIconButton />
-          <AppIconButton />
-          <AppIconButton />
-          <AppIconButton />
+          <AppIconButton v-for="tag in tags" :app="tag" />
         </div>
       </div>
     </div>
     <div class="mx-auto max-w-[816px] px-8 pb-10">
       <ContentDoc id="nuxt_content" />
-      <div class="mt-10 flex justify-between gap-4">
-        <div
+      <div
+        class="mt-10 flex justify-between gap-4"
+        :class="{ '!justify-end': !previousLesson }"
+      >
+        <nuxt-link
+          v-if="previousLesson"
+          :to="previousLesson._path"
           class="cursor-pointer rounded border border-zinc-300 px-4 py-3 transition-all hover:border-zinc-400 hover:bg-zinc-50"
         >
           <div class="mb-1 text-right text-xs font-medium text-zinc-700">
-            Introdução
+            Aula Anterior
           </div>
           <div class="flex items-center justify-end gap-2">
             <font-awesome-icon class="text-sm" icon="arrow-left" />
-            <span class="text-sm font-medium leading-[14px]"
-              >Antes de começar</span
-            >
+            <span class="text-sm font-medium leading-[14px]">
+              {{ previousLesson.title }}
+            </span>
           </div>
-        </div>
+        </nuxt-link>
         <div
+          v-if="nextLesson"
           @click="finishLesson = true"
           class="cursor-pointer rounded border border-zinc-300 px-4 py-3 transition-all hover:border-zinc-400 hover:bg-zinc-50"
         >
-          <div class="mb-1 text-xs font-medium text-zinc-700">Introdução</div>
+          <div class="mb-1 text-xs font-medium text-zinc-700">Próxima aula</div>
           <div class="flex items-center gap-2">
-            <span class="text-sm font-medium leading-[14px]"
-              >Antes de começar</span
-            >
+            <span class="text-sm font-medium leading-[14px]">
+              {{ nextLesson.title }}
+            </span>
             <font-awesome-icon class="text-sm" icon="arrow-right" />
           </div>
         </div>
@@ -96,17 +117,22 @@ const { description, image, title } = data.value.data.content._dir;
       <div
         class="mt-6 flex items-center justify-center gap-2 border-t border-dashed border-zinc-300 pt-6"
       >
+        <nuxt-link
+          :to="`https://github.com/menthorlabs/courses/edit/main/${currentLesson._file}`"
+          target="_blank"
+        >
+          <MButton
+            icon-left="edit"
+            size="sm"
+            variant="inherit"
+            text="Editar no GitHub"
+          />
+        </nuxt-link>
         <MButton
-          icon-left="edit"
+          icon-left="hashtag"
           size="sm"
           variant="inherit"
-          text="Editar no GitHub"
-        />
-        <MButton
-          icon-left="share"
-          size="sm"
-          variant="inherit"
-          text="Compartilhar"
+          text="Construa em Público"
         />
         <MButton
           icon-left="star"
@@ -146,6 +172,7 @@ const { description, image, title } = data.value.data.content._dir;
       </div>
       <div
         class="group relative mb-2 mr-2 mt-7 inline-flex items-center justify-center overflow-hidden rounded-lg bg-gradient-to-br from-blue-400 to-pink-400 p-0.5 text-sm font-medium text-gray-900"
+        v-if="!isEnrolled"
       >
         <div
           class="to-pink-400-10 relative flex items-center gap-4 rounded-md bg-white bg-gradient-to-br from-blue-400/20 to-pink-400/20 p-6"
