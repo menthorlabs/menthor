f8a61ce1df165821334b0aae2107df69ab00e692
Bernardo Simonassi <bernardosimonassi@hotmail.com>
2023-07-04 00:20:52 -0300

    refactor: validators to typescript

---
+++ apps/app/composables/useFormValidator.ts
@@ -0,0 +1,55 @@
+import validators from "../../../packages/utils/validators";
+
+export function useFormValidator() {
+  function allRules(required: boolean, rules: string[]) {
+    if (required && !rules.includes("required")) {
+      return ["required", ...rules];
+    }
+
+    return rules;
+  }
+
+  function validate(
+    value: string,
+    rules: Array<string | { isValid: Function; errorMessage: string }>
+  ) {
+    const errorMessage = ref("");
+    if (!Array.isArray(rules) || rules.length <= 0) return true;
+    if ((!value || value?.length <= 0) && !rules.includes("required"))
+      return true;
+
+    const failedRule = rules.find(
+      (rule: string | { isValid: Function; errorMessage: string }) => {
+        if (
+          typeof rule === "function" ||
+          (validators[rule as "email" | "password" | "required"] == null &&
+            typeof rule !== "object")
+        ) {
+          return false;
+        }
+
+        if (typeof rule === "object") {
+          return !rule.isValid(value);
+        }
+        return (
+          validators[rule as "email" | "password" | "required"](value)
+            .isValid === false
+        );
+      }
+    );
+
+    if (failedRule) {
+      if (typeof failedRule === "object") {
+        errorMessage.value = failedRule.errorMessage;
+        return !failedRule;
+      }
+      const failedValidator =
+        validators[failedRule as "email" | "password" | "required"](value);
+      errorMessage.value = failedValidator.errorMessage;
+    }
+
+    return { isValid: !failedRule, errorMessage: errorMessage.value };
+  }
+
+  return { validate, allRules };
+}
--- packages/utils/validators.ts
+++ packages/utils/validators.ts
@@ -1,5 +1,6 @@
+/* eslint-disable no-unused-vars */
 export default {
-  email: (value) => {
+  email: (value: string | boolean) => {
     return {
       isValid:
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
@@ -8,17 +9,20 @@ export default {
       errorMessage: "Insira um email válido.",
     };
   },
-  password: (value) => {
+  password: (value: string | boolean) => {
     return {
       isValid: /^(?=.*[0-9])(?=.*[!@#$%^&*_+=]).{8,42}$/.test(`${value}`),
       errorMessage:
         "Sua senha deve ter pelo menos 8 caracteres, 1 caractere especial (!@#$%^&*_+=) e 1 letra maiúscula.",
     };
   },
-  required: (value) => {
+  required: (value: string | boolean) => {
     return {
-      isValid: value?.length > 0 || value === true,
+      isValid: String(value).length > 0 || value === true,
       errorMessage: "Esse campo é obrigatório.",
     };
   },
-};
+} as Record<
+  "email" | "password" | "required",
+  (value: string | boolean) => { isValid: boolean; errorMessage: string }
+>;
